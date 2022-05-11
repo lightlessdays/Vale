9d852375df686dc9e97c71ad71aff75598a66219
dhruv badaya <dhruvbadaya@gmail.com>
2022-05-12 01:39:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,39 @@
 //code cleanup completed
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:valecode/pages/homepage/homepage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     systemNavigationBarColor: Colors.black87,
     statusBarColor: Colors.black,
   ));
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget { const
+class MyApp extends StatelessWidget {
   MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Vale',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          secondary: Colors.black, // this is the replacement of accent color
+    return ScreenUtilInit(
+      builder: (_)=> MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Vale',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            secondary: Colors.black, // this is the replacement of accent color
+          ),
         ),
+        home: HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
